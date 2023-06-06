af6757902e2aedebafb6864b8781c91808ff64d0
Pablo Fernández del Amo <109191044+Pablxfdez@users.noreply.github.com>
2023-06-07 01:01:24 +0200

    Arreglo_Fallos_Practica

--- FernandezdelAmoP_Practica1.2.py
+++ FernandezdelAmoP_Practica1.2.py
@@ -1,87 +1,87 @@
+
 # -*- coding: utf-8 -*-
 
-#Práctica 1, Parte Opcional: PABLO FERNÁNDEZ DEL AMO  04231435X
+#Práctica 1, Parte Obligatoria: PABLO FERNÁNDEZ DEL AMO  04231435x
 
 from multiprocessing import Process, current_process
-from multiprocessing import Semaphore
+from multiprocessing import Semaphore, Lock, Value
 from multiprocessing import Manager
 from time import sleep 
 from random import randint, random
 
+N = 8 # number of integers produced by each producer
+NProducers = 3 # Number of producers
 
-N = 10 # number of integers produced by each producer
-NProducers = 5 # Number of producers
-
-Buffer_size = 7 # size of the buffer of each producer, difference with part 1
+# Buffer_size = 1 , in this case our buffer is of length one, so using Lock() instead of Semaphore is ok.
 
 
 def delay(factor = 3):
     sleep(random()/factor)
 
 
+
 def producer(storage, empty, non_empty, prod_number):
-    data = -1
-    for iter in range(N): 
+    data = 0
+    for _ in range(N): 
         data = randint(data + 1, data + 40) 
         print(f"Producer {current_process().name} has produced the number: {data}")
-        empty[prod_number].acquire() # the producer acquires the lock to write to the buffer
-        try:
-            if storage[prod_number][0] == -2:
-                storage[prod_number].pop()
-            storage[prod_number].append(data)
-            print(f"Producer {current_process().name} has stored the number: {data}")
-            delay()
-        finally:
-            non_empty[prod_number].release() # the producer releases the semaphore to signal to the collector that there is data in the buffer
-    
-    empty[prod_number].acquire() # acquire the lock to write to the buffer
-    try:    
-        storage[prod_number].append(-1) # write -1 to indicate that the producer has finished
+
+       
+        storage[prod_number].value = data
+        print(f"Producer {current_process().name} has stored the number: {data}")
+        empty[prod_number].release()
+        non_empty[prod_number].acquire() # the producer acquires the lock to write to the buffer
+        delay()
+
+    try:
+        storage[prod_number].value = -1 # write -1 to indicate that the producer has finished
     finally:
-        non_empty[prod_number].release() # release the semaphore to signal to the collector that there is data in the buffer
+        empty[prod_number].release() # release the semaphore to signal to the collector that there is data in the buffer
         print(f"The producer {current_process().name} has finished producing") # print message to indicate that the producer has finished producing
+
 
 
 def minimum(storage):
     index = -1 # Initializes the index of the minimum value
-    boolean = False # Boolean variable to check if there are elements in the storage
+    finished = True # Boolean variable to check if there are elements in the storage
     for pos, val in enumerate(storage):
-        if val[0] >= 0:
-            if not boolean:
-                boolean = True
+        if val.value >= 0:
+            if finished:
+                finished = False
                 index = pos
-            elif val[0] < storage[index][0]:
+            elif val.value < storage[index].value:
                 index = pos
-    return (boolean, index) # Returns a tuple with a boolean indicating if there are non-negative values in the storage and the index of the smallest value.
+
+    return finished, index, storage[index].value # Returns a tuple with a boolean indicating if there are non-negative values in the storage and the index of the smallest value.
 
 
 
 def merge(storage, empty, non_empty, merged_products):
     # Wait for all producers to produce something
-    for sem in non_empty:
+    for sem in empty:
         sem.acquire()
     
     # Iterate while there are still producers producing numbers
-    while (n:=minimum(storage))[0]:
+    finished = False
+    while not finished:
+        finished, index, val = minimum(storage)
         # Print a message indicating that the collector is choosing a number to append
         print('Collector choosing a number from storage to append...')
-        try: 
-            # Get the data from the storage array and set it to -2
-            data = storage[n[1]].pop(0)
-            if not storage[n[1]]:
-                storage[n[1]].append(-2)
+        if not finished:
+            try: 
+                # Get the data from the storage array and set it to -2
+                merged_products.append(val)
+                print(f"Collector has picked up and appended the number: {val}.")
+                print('Ordering...')
+                delay()
+                
+            finally:
+
+                non_empty[index].release()
+                empty[index].acquire()
+                
+                
             
-            # Append the data to the merged products list and print a message indicating it was appended
-            merged_products.append(data)
-            print(f"Collector has picked up and appended the number: {data}.")
-            delay()
-            
-        finally:
-            # Release the empty lock for the chosen index
-            empty[n[1]].release()
-            
-            # Acquire the non_empty semaphore for the chosen index
-            non_empty[n[1]].acquire()
 
 
 
@@ -89,22 +89,20 @@ def main():
     # Use a Manager() to share information between processes
     manager = Manager()
 
-    # Create shared array with NProd elements initialized to -2
-    storage = manager.list()
+    # Create shared array with NProd elements initialized to 0
+    storage = [Value('i', 0) for i in range(NProducers)]
 
     # Create a shared list to store merged products
     merged_products = manager.list()
 
-    # Two lists of semaphores foe each task
-    non_empty = [Semaphore(0) for _ in range(NProducers)]
-    empty = [Semaphore(Buffer_size) for _ in range(NProducers)] # In this section, we change the Lock() and use the Semaphore with size NBuffer,
-    processes = []                                                  # as it is going to dictaminate the size of the buffer (array) associated to producer i.
-    
+    # One list of semaphores and the other of Locks
+    non_empty = [Semaphore(0) for _ in range(NProducers)] # marks if the storage for Prod_i is full
+    empty = [Semaphore(0) for _ in range(NProducers)] # marks if the storage for Prod_i is empty
+    processes = []
+
     # Create NProd producer processes and append them to processes list
     for prod_number in range(NProducers):
-        storage.append(manager.list())
-        storage[prod_number].append(-2)
-        processes.append(Process(target=producer, name=f"Producer_{prod_number}",
+        processes.append(Process(target=producer, name=f"Prod_{prod_number}",
                                   args=[storage, empty, non_empty, prod_number]))
         
     # Create a consumer process and append it to processes list
@@ -112,10 +110,7 @@ def main():
                              args=[storage, empty, non_empty, merged_products]))
     
     print(f"Merged products before execution: {list(merged_products)}")
-    print("Storage before execution: \n[", end = ' ')
-    for i in storage:
-        print(i[:], end =' ')
-    print(']')
+    print(f"Storage before execution: {list(storage)}")
 
     # Start all processes
     for p in processes:
@@ -125,10 +120,7 @@ def main():
         p.join()
 
     # Print the storage and merged products lists before and after execution
-    print("Storage after execution: \n[", end = ' ')
-    for i in storage:
-        print(i[:], end =' ')
-    print(']')
+    print(f"Storage after execution: {list(storage)}")
     
 
     # Print the merged products list after execution
